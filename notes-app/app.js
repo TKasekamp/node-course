c07715466f0e02b33c28d6d1d9ccca8954433bd7
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-03-13 22:33:19 +0200

    More chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -6,5 +6,9 @@ console.log(createNotes());
 console.log(validator.isEmail('asd@ad.com'));
 console.log(validator.isURL('ads'));
 
-console.log(`${chalk.green('Success!')} ${chalk.bold('bold')}`)
+console.log(`${chalk.green('Success!')} ${chalk.red.inverse.bold('bold')}`)
 
+const log = console.log;
+log(chalk.keyword('orange')('Yay for orange colored text!'));
+log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+log(chalk.hex('#DEADED').bold('Bold gray!'));
