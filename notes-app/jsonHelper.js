3494c276aa06ec983124ebf586d2aa43f88ffd5b
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-03-16 17:40:16 +0200

    Complete json challenge
    Done lec 4 sec 18

---
+++ notes-app/jsonHelper.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const fileName = '1-json.json'
+const buffered = fs.readFileSync(fileName);
+
+const data = JSON.parse(buffered.toString());
+
+console.log(data);
+const mydata = {...data, name: "Tõnis", age: 25};
+
+fs.writeFileSync(fileName, JSON.stringify(mydata));
