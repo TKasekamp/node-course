c245d452ad2d42a97e8de75f0a800b43e8ae2da1
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-03-16 17:16:28 +0200

    Define commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,41 +5,31 @@ const yargs = require('yargs');
 const {createNotes} = require('./notes')
 console.log(createNotes());
 
-const val = () => {
-  console.log(validator.isEmail('asd@ad.com'));
-  console.log(validator.isURL('ads'));
-}
-
-const printChalk = () => {
-  console.log(`${chalk.green('Success!')} ${chalk.red.inverse.bold('bold')}`)
-
-  const log = console.log;
-  log(chalk.keyword('orange')('Yay for orange colored text!'));
-  log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
-  log(chalk.hex('#DEADED').bold('Bold gray!'));
-}
-
-// const command = process.argv[2]
-//
-// if (command === 'add') {
-//   console.log('adding note')
-// } else if (command === 'remove') {
-//   console.log('Removing note')
-// }
+const commands = [
+  {
+    command: 'add',
+    describe: 'add a new note',
+    handler: () => console.log('adding new note')
+  },
+  {
+    command: 'remove',
+    describe: 'remove new note',
+    handler: () => console.log('removing new note')
+  },
+  {
+    command: 'list',
+    describe: 'list all notes',
+    handler: () => console.log('listing notes')
+  },
+  {
+    command: 'read',
+    describe: 'read note',
+    handler: () => console.log('reading notes')
+  }
+]
 
 yargs.version('1.1.0')
 
-yargs.command({
-  command: 'add',
-  describe: 'add a new note',
-  handler: () => console.log('adding new note')
-})
-
-yargs.command({
-  command: 'remove',
-  describe: 'remove new note',
-  handler: () => console.log('removing new note')
-})
+commands.forEach(c => yargs.command(c))
 
 console.log(yargs.argv)
-
