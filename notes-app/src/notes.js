a563236243ae95145bae9c32d98f042541a9bf58
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-03-16 19:28:03 +0200

    Init weather app

--- notes-app/src/notes.js
+++ notes-app/src/notes.js
@@ -5,48 +5,48 @@ const filePath = 'notes.json';
 
 const loadNotes = () => {
   try {
-    const databuffer = fs.readFileSync(filePath)
+    const databuffer = fs.readFileSync(filePath);
     return JSON.parse(databuffer.toString())
   } catch (e) {
     return []
   }
-}
+};
 
 const writeNotes = (notes) => {
   fs.writeFileSync(filePath, JSON.stringify(notes));
-}
+};
 
 const addNote = (note) => {
-  const notes = loadNotes()
-  const duplicateNote = notes.find(n => n.title === note.title)
+  const notes = loadNotes();
+  const duplicateNote = notes.find(n => n.title === note.title);
 
   if (!duplicateNote) {
-    writeNotes([...notes, note])
+    writeNotes([...notes, note]);
     console.log(chalk.green('Note added'))
   } else {
     console.log(chalk.red(`Note ${note.title} taken!`))
   }
 
-}
+};
 
 const listNotes = () => {
-  const notes = loadNotes()
+  const notes = loadNotes();
   notes.forEach(note => {
     console.log(`${chalk.bold('Note:')} ${note.title}`)
   })
-}
+};
 
 const removeNote = (title) => {
-  const notes = loadNotes()
+  const notes = loadNotes();
 
-  const filteredNotes = notes.filter(n => n.title !== title)
+  const filteredNotes = notes.filter(n => n.title !== title);
   if (notes.length === filteredNotes.length) {
     console.log(chalk.red.inverse(`Found no notes to remove!`))
   } else {
     console.log(chalk.green.inverse(`Removing note ${title}`))
   }
   writeNotes(filteredNotes)
-}
+};
 
 const getNote = (title) => {
   const notes = loadNotes();
@@ -57,7 +57,7 @@ const getNote = (title) => {
     console.log(chalk.red(`No note found!`))
   }
 
-}
+};
 
 module.exports = {
   addNote,
