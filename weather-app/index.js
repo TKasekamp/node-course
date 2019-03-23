f82c88af421921f639b53577154bee35ef2a5fa0
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-03-23 12:10:33 +0200

    Darksky improvements

--- weather-app/index.js
+++ weather-app/index.js
@@ -1,10 +1,26 @@
 const axios = require('axios');
 
-const url = 'https://api.darksky.net/forecast/29319c2379e6494906970d6add51aa00/37.8267,-122.4233';
+const darkSky = 'https://api.darksky.net/forecast/29319c2379e6494906970d6add51aa00';
 
-axios.get(url).then(
-  response => {
-    const data = response.data;
-    console.log(data.currently)
-  }
-)
+const formatData = ({currently, daily}) => {
+  return `${daily.summary} It is currently ${currently.apparentTemperature} degrees out. There is ${currently.precipProbability} % chance of rain`;
+};
+
+
+
+const params = {
+  units: 'si',
+  lang: 'et'
+};
+
+const darkSkyRequest = (latitude, longitude) => {
+  const fullUrl = `${darkSky}/${latitude},${longitude}`;
+  return axios.get(fullUrl, {params}).then(
+    response => {
+      const data = response.data;
+      console.log(formatData(data))
+    }
+  );
+}
+
+darkSkyRequest('37.8267', '-122.4233');
