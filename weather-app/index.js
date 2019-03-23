f714fcbd12f21d648264767915cb9fc916e9449d
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-03-23 12:28:56 +0200

    Mapbox request

--- weather-app/index.js
+++ weather-app/index.js
@@ -6,14 +6,16 @@ const formatData = ({currently, daily}) => {
   return `${daily.summary} It is currently ${currently.apparentTemperature} degrees out. There is ${currently.precipProbability} % chance of rain`;
 };
 
+const access_token = 'pk.eyJ1IjoiZWlvczIxIiwiYSI6ImNqdGxjMGF5NTFleXYzeW82bjJoeDhncjgifQ.5WyuxaXZgOC0-w0wyZIC5g';
 
+const mapBox = 'https://api.mapbox.com/geocoding/v5/mapbox.places';
 
 const params = {
   units: 'si',
   lang: 'et'
 };
 
-const darkSkyRequest = (latitude, longitude) => {
+const darkSkyRequest = ({latitude, longitude}) => {
   const fullUrl = `${darkSky}/${latitude},${longitude}`;
   return axios.get(fullUrl, {params}).then(
     response => {
@@ -21,6 +23,31 @@ const darkSkyRequest = (latitude, longitude) => {
       console.log(formatData(data))
     }
   );
-}
+};
 
-darkSkyRequest('37.8267', '-122.4233');
+
+const mapBoxRequest = (searchTerm = 'Los%20Angeles') => {
+  const fullUrl = `${mapBox}/${searchTerm}.json`;
+  return axios.get(fullUrl, {params: {access_token, limit: 1, language: 'et'}}).then(
+    response => {
+      const data = response.data;
+      console.log(data.features[0])
+      return data;
+    }
+  );
+};
+
+// darkSkyRequest('37.8267', '-122.4233');
+
+mapBoxRequest().then(
+  data => {
+    const longitude = data.features[0].center[0];
+    const latitude = data.features[0].center[1];
+    return {latitude, longitude};
+  }
+).then(
+  coords => {
+    console.log(coords)
+    return darkSkyRequest(coords)
+  }
+);
