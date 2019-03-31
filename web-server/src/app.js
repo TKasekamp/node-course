10731b32eb1e1662eda2a0bf5fdfc6cd343c337f
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-03-31 15:48:41 +0300

    Some routes
    Section 7, lecture 44

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,7 +6,20 @@ app.get('', (req, res) => {
   res.send('hello express')
 });
 
+app.get('/help', (req, res) => {
+  res.send('<h1>help</h1>')
+});
 
+app.get('/about', (req, res) => {
+  res.send('<h1>About page</h1>')
+});
+
+app.get('/weather', (req, res) => {
+  res.send({
+    forecast: 'bad',
+    location: 'Tartu'
+  })
+});
 
 app.listen(3000, () => {
   console.log('started')
