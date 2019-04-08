5e05b43f1bdda2427d61c288f34da842ee9de357
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-04-08 20:27:50 +0300

    Lecture 76

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -4,6 +4,14 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
+
+const callback = (error, result) => {
+  if (error) {
+    return console.log('unable to insert')
+  }
+  console.log(result.ops)
+};
+
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
   if (error) {
     return console.log('Unable to connect')
@@ -12,8 +20,20 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
   console.log('connected');
   const db = client.db(databaseName);
 
-  db.collection('users').insertOne({
-    name: 'Tõnis',
-    age: 25
-  })
+  // db.collection('users').insertOne({
+  //   name: 'Tõnis',
+  //   age: 25
+  // }, callback)
+
+  // const docs = [{name: 'Jen', age: 28}, {name: 'Gunther', age: 43}];
+  // db.collection('users').insertMany(docs, callback)
+  insertTasks(db)
 });
+
+const insertTasks = (db) => {
+  const docs = [{description: 'Hello', completed: true},
+    {description: 'World', completed: false},
+    {description: 'Long way round', completed: false}
+    ];
+  db.collection('tasks').insertMany(docs, callback)
+}
