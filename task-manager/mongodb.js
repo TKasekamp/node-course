044cb1096b3fdecc654bc98bb15e8569e53fe776
Tõnis Kasekamp <toniskasekamp@gmail.com>
2019-04-08 20:50:29 +0300

    Lecture 78

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,15 +1,18 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
+const id = new ObjectID();
+console.log(id)
 
 const callback = (error, result) => {
   if (error) {
     return console.log('unable to insert')
   }
-  console.log(result.ops)
+  console.log(result)
 };
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
@@ -21,19 +24,30 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
   const db = client.db(databaseName);
 
   // db.collection('users').insertOne({
+  //   _id: id,
   //   name: 'Tõnis',
   //   age: 25
   // }, callback)
 
   // const docs = [{name: 'Jen', age: 28}, {name: 'Gunther', age: 43}];
   // db.collection('users').insertMany(docs, callback)
-  insertTasks(db)
+  // insertTasks(db)
+  lastTask(db)
+  notCompleted(db)
 });
 
 const insertTasks = (db) => {
   const docs = [{description: 'Hello', completed: true},
     {description: 'World', completed: false},
     {description: 'Long way round', completed: false}
-    ];
+  ];
   db.collection('tasks').insertMany(docs, callback)
+};
+
+const lastTask = (db) => {
+  db.collection('tasks').findOne({_id: new ObjectID("5cab8452a57586227e2c1c95")}, callback)
+}
+
+const notCompleted = (db) => {
+  db.collection('tasks').find({completed: false}).toArray(callback)
 }
